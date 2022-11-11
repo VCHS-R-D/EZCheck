0b8ab6fdbc1a883064a26b7becc2236b6d5466be
Advay Balakrishnan <a.balakrishnan@warriorlife.net>
2022-11-10 23:31:06 -0800

    merge conflicts resolved

--- server/routers.go
+++ server/routers.go
@@ -7,14 +7,7 @@ import (
 func Init() {
 	e := echo.New()
 
-<<<<<<< HEAD
-	e.POST("/register/admin", registerAdminRouter)
-	e.POST("/register/student", registerStudentRouter)
+	// g := e.Group("/admin")
 
-	g := e.Group("/admin")
-	g.Use(middleware(BasicAuth())
-
-=======
->>>>>>> eb011a258ab3e973868b3652481219b572955586
 	e.Logger.Fatal(e.Start(":3000"))
 }
