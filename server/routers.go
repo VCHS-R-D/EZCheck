4e4698d829e8882b6e3b368257a4ada6290868b6
Akhil Datla <66145155+akhil-datla@users.noreply.github.com>
2022-11-13 17:18:52 -0800

    Fix API restrictions

--- server/routers.go
+++ server/routers.go
@@ -2,6 +2,7 @@ package server
 
 import (
 	"fmt"
+	"net/http"
 
 	"github.com/labstack/echo/v4"
 	"github.com/labstack/echo/v4/middleware"
@@ -14,12 +15,25 @@ func Init(port string) {
 
 	e.Use(middleware.Logger())
 	e.Use(middleware.Recover())
+	e.Use(middleware.CORSWithConfig(middleware.CORSConfig{
+		Skipper:          middleware.DefaultSkipper,
+		AllowOrigins:     []string{"*"},
+		AllowCredentials: true,
+		AllowMethods:     []string{http.MethodGet, http.MethodHead, http.MethodPut, http.MethodPatch, http.MethodPost, http.MethodDelete, http.MethodOptions},
+	}))
 
+	// FOR MACHINES
 	e.POST("/auth", Authenticate)
+	e.POST("/signout", SignOut)
+
+	// AUDIT LOG
 	e.GET("/log", ReadLog)
 
+	// REGISTER NEW ADMINS AND USERS
+	e.POST("/admin/create", CreateAdmin)
+	e.POST("/user/create", CreateUser)
+
 	gAdmin := e.Group("/admin", middleware.BasicAuth(AdminAuth))
-	gAdmin.POST("/create", CreateAdmin)
 	gAdmin.POST("/get", GetAdmin)
 	gAdmin.POST("/certify", CertifyUser)
 	gAdmin.POST("/uncertify", UncertifyUser)
@@ -27,13 +41,11 @@ func Init(port string) {
 	gAdmin.DELETE("/delete", DeleteAdmin)
 	gAdmin.POST("/machines/create", CreateMachine)
 	gAdmin.POST("/machines/get", GetMachines)
-	gAdmin.POST("/machines/signout", SignOut)
 	gAdmin.DELETE("/machines/delete", DeleteMachine)
 	gAdmin.POST("/machines/actions/add", AddAction)
 	gAdmin.POST("/machines/actions/delete", DeleteAction)
 
 	gUser := e.Group("/user", middleware.BasicAuth(UserAuth))
-	gUser.POST("/create", CreateUser)
 	gUser.POST("/get", GetUser)
 	gUser.DELETE("/delete", DeleteUser)
 
