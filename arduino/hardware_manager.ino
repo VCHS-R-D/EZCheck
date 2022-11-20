9e180d079a2b6f2e59d171721328b65702337f96
Advay Balakrishnan <a.balakrishnan@warriorlife.net>
2022-11-19 20:57:33 -0800

    testing code ready for deploying

--- arduino/hardware_manager.ino
+++ arduino/hardware_manager.ino
@@ -5,21 +5,48 @@
 
 #define BUTTON_PIN 2
 #define LED_PIN 3
+#include <Keypad.h>
 
-const char* SSID = "REPLACE_WITH_YOUR_SSID";
-const char* PASSWORD = "REPLACE_WITH_YOUR_PASSWORD";
-const char* URL = "http://192.168.1.106:8000";
+#include <LiquidCrystal.h>
+
+//Keypad vars
+const int ROW_NUM = 4; //four rows
+const int COLUMN_NUM = 4; //four columns
+
+char keys[ROW_NUM][COLUMN_NUM] = {
+  {'1','2','3', 'A'},
+  {'4','5','6', 'B'},
+  {'7','8','9', 'C'},
+  {'*','0','#', 'D'}
+};
+
+byte pin_rows[ROW_NUM] = {36, 34, 32, 30}; //connect to the row pinouts of the keypad
+byte pin_column[COLUMN_NUM] = {28, 26, 24, 22}; //connect to the column pinouts of the keypad
+
+Keypad keypad = Keypad( makeKeymap(keys), pin_rows, pin_column, ROW_NUM, COLUMN_NUM );
+
+//LCD vars
+const int rs = 12, en = 11, d4 = 5, d5 = 4, d6 = 3, d7 = 2;
+LiquidCrystal lcd(rs, en, d4, d5, d6, d7);
+
+const char* PASSWORD = "***REMOVED***";
+const char* URL = "http://:8000";
+const int LENGTH = 6;
+
+String keypad_buffer;
 
 HTTPClient arduino_client;
 WiFiClient wifi_client;
 
 void setup() {
+    keypad_buffer = "";
     // light protoype with LED
     setMode(LED_PIN, OUTPUT);
     setMode(BUTTON_PIN, INPUT);
+
     arduino_client.begin()
 
-    Serial.begin(115200); 
+    Serial.begin(9600); 
     try {
         WiFi.begin(ssid, password);
         Serial.println("Connecting");
@@ -35,25 +62,34 @@ void setup() {
     Serial.println("Timer set to 5 seconds (timerDelay variable), it will take 5 seconds before publishing the first reading.");
 }
 
+
 void loop() {
-    if ((millis() - lastTime) > timerDelay) {
+    char key = keypad.getKey();
+    if (key) {
+        if key == 'D' && keypad_buffer.length() > 0 {
+            keypad_buffer.remove(keypad_buffer.length() - 1);
+        } else {
+            keypad_buffer.concat(key);
 
-      WiFiClient client;
-      HTTPClient http;
-    
-      http.begin(client, URL);
-      
-      http.addHeader("Content-Type", "application/x-www-form-urlencoded");
-      
-      char* httpRequestData = "{/"code/": /"000000/"}";           
+            if(keypad_buffer.length() == 6) {
+                WiFiClient client;
+                HTTPClient http;
 
-      int httpResponseCode = http.POST(httpRequestData);
-      
-      Serial.print("HTTP Response code: ");
-      Serial.println(httpResponseCode);
-        
-      // Free resources
-      http.end();
+                http.begin(client, URL);
+                
+                http.addHeader("Content-Type", "application/x-www-form-urlencoded");
+                
+                String httpRequestDataJSON = "{/"code/": /"" + keypad_buffer + "/"}";           
+
+                int httpResponseCode = http.POST(httpRequestDataJSON);
+                
+                Serial.print("HTTP Response code: ");
+                Serial.println(httpResponseCode);
+                
+                // Free resources
+                http.end();
+            }
+        }
     }
 
 }
