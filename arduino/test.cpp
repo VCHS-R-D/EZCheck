78905182352d04aebc44bec8705d810ca05513ab
Advay Balakrishnan <a.balakrishnan@warriorlife.net>
2022-11-28 14:24:11 -0800

    deleted unecessary files

--- arduino/test.cpp
+++
@@ -1,9 +0,0 @@
-#include <bits/stdc++.h>
-
-using namespace std;
-
-int main() {
-    string keypad_buffer = "000000";
-    string test = "{\"code\": " + keypad_buffer + "\"}";
-    cout << test << endl;
-}
