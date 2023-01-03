6463959a3faed5d65289f1511abcb1a548e9674f
Brayden <brayden.tam@gmail.com>
2023-01-03 15:48:10 -0800

    create/delete machines

--- frontend/ezcheck/src/components/MachineList.js
+++ frontend/ezcheck/src/components/MachineList.js
@@ -1,8 +1,11 @@
 import React from 'react'
+import {Cookies, useCookies} from 'react-cookie';
 import axios from "axios";
 
 function MachineList(){
   const [machineList, setMachineList] = React.useState([]);
+  const [machineID, setMachineID] = React.useState("");
+  const [cookie, setCookie,removeCookie] = useCookies('user');
   React.useEffect(() => {getMachines()}, [])
   function getMachines() {
     var config = {
@@ -14,6 +17,7 @@ function MachineList(){
     axios(config)
     .then(function (response) {
       setMachineList(response.data);
+      console.log(response.data);
       
     })
     .catch(function (error) {
@@ -21,9 +25,57 @@ function MachineList(){
     });
     
   }
+
+  function createMachine() {
+    var formdata = new FormData();
+    formdata.append("machineID", machineID);
+    var config = {
+    method: 'post',
+    url: 'http://localhost:8080/admin/machines/create',
+    headers: {
+        'Authorization': `Basic ${cookie.authToken}`,
+    },
+    data : formdata
+    };
+    axios(config)
+    .then(function (response) {
+        alert(response.data);
+        getMachines();        
+    })
+    .catch(function (error) {
+    console.log(error);
+    });
+    }
+
+    function deleteMachine() {
+      var formdata = new FormData();
+      formdata.append("machineID", machineID);
+      console.log(machineID);
+      var config = {
+      method: 'delete',
+      url: 'http://localhost:8080/admin/machines/delete',
+      headers: {
+          'Authorization': `Basic ${cookie.authToken}`,
+      },
+      data : formdata
+      };
+      axios(config)
+      .then(function (response) {
+          alert(response.data);
+          getMachines();        
+      })
+      .catch(function (error) {
+      console.log(error);
+      });
+      }
+
     return( <>
       <div>
-      {machineList.map(machine => (<div key={machine.id}>{machine.name} in-use: {String(machine.in_use)} actions: ({machine.actions})</div>))}
+        <form>
+      <input placeholder="Machine ID" onChange={(event) => {setMachineID(event.target.value)}}></input>
+      </form>
+      <button onClick={() => {createMachine()}}>Create Machine</button>
+      {machineList.map(machine => (<div key={machine.id}>{machine.id} in-use: {String(machine.in_use)} actions: ({machine.actions}) <button key={machine.id} onClick={() => {setMachineID(machine.id); console.log(machine.id); deleteMachine();}}>DELETE</button></div>))}
       </div>
     </>
   )
