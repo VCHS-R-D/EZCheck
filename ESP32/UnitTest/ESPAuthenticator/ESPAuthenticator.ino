d9d9d3e99463c66aef4e885fd592cc7423f3b7a1
3LucasZ <lucas.j.zheng@gmail.com>
2023-01-18 17:37:36 -0800

    Newest edition of ESP Authenticator

--- ESP32/UnitTest/ESPAuthenticator/ESPAuthenticator.ino
+++ ESP32/UnitTest/ESPAuthenticator/ESPAuthenticator.ino
@@ -13,29 +13,35 @@ char keys[ROW_NUM][COLUMN_NUM] = {
   {'*','0','#', 'D'}
 };
 
-byte pin_rows[ROW_NUM] = {17, 5, 18, 19}; //connect to the row pinouts of the keypad
-byte pin_column[COLUMN_NUM] = {15, 2, 4, 16}; //connect to the column pinouts of the keypad
+byte pin_rows[ROW_NUM] = {17, 5, 18, 19}; //connect to the row pinouts of the keypad {R1, R2, R3, R4}
+byte pin_column[COLUMN_NUM] = {15, 2, 4, 16}; //connect to the column pinouts of the keypad {C1, C2, C3, C4}
 
 Keypad keypad = Keypad( makeKeymap(keys), pin_rows, pin_column, ROW_NUM, COLUMN_NUM);
 
-
 //LCD vars
-const int rs = 33, en = 25, d4 = 26, d5 = 27, d6 = 14, d7 = 12;
+const int rs = 33, en = 25, d4 = 26, d5 = 27, d6 = 14, d7 = 32;
 LiquidCrystal lcd(rs, en, d4, d5, d6, d7);
+
+//LED vars
+const int led  =  21; 
 
 //Pass vars
 String pass = "";
 
 void setup(){
   Serial.begin(9600);
+  //Initial state
   lcd.begin(16, 2);
   lcd.print("Welcome!");
+  pinMode(led,OUTPUT); 
+  digitalWrite(led,LOW);
 }
 
 void loop(){
   //handle keypress
   char key = keypad.getKey();
   if (key){
+    digitalWrite(led,LOW);
     //D -> remove last char
     if (key=='D') {
       pass.remove(pass.length()-1);
@@ -46,7 +52,12 @@ void loop(){
     //* -> submit pass
     else if (key=='*') {
       lcd.clear();
-      if (pass.length()==6) lcd.print("Hello Mr. Huber");
+      // correct pass
+      if (pass.length()==6) {
+        lcd.print("Hello Mr. Huber");
+        digitalWrite(led,HIGH);
+      }
+      //wrong pass
       else lcd.print("Denied Access");
       pass="";
     }
